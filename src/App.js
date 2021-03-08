38744f49b22e8f545fbb52e3bce4162bf5c2c520
gargashna <ashnaagg2@gmail.com>
2021-03-08 14:01:27 -0500

    "com4"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import marked from 'marked';
+import ReactMarkdown from 'react-markdown'
 function App() {
-  return (
+
+  const [markdown, setMarkdown] = useState('#sup');
+  function handleChange(e){
+      setMarkdown(e.target.value)
+  }
+ return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea onChange={handleChange} value={markdown}/>
+      
+      {/* <div className="preview">{markdown}</div> */}
+   
+    
+    <ReactMarkdown className="preview" source={markdown}/>
     </div>
   );
 }
